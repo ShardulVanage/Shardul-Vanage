46e2ca54b32f8d9633b59636de1422d8ddba58b5
Shardul <66526618+ShardulVanage@users.noreply.github.com>
2022-08-10 12:25:55 +0530

    portfolio bug fixed

--- porfolio/src/assets/components/Portfolio.jsx
+++ porfolio/src/assets/components/Portfolio.jsx
@@ -16,7 +16,7 @@ const Portfolio = () => {
       {
         id: 2,
         src: foodiz,
-        text:"FoOdie website"
+        text:"FoOdie_website"
       },
       {
         id: 3,
@@ -26,12 +26,12 @@ const Portfolio = () => {
       {
         id: 4,
         src: video_game,
-        text:"Video Game Info App"
+        text:"Video_Game_Info_App"
       },
       {
         id: 5,
         src: food_delivery,
-        text:"Online food delivery App"
+        text:"Online_food_delivery_App"
       },
       {
         id: 6,
@@ -42,42 +42,39 @@ const Portfolio = () => {
   
     return (
       <div
-        name="portfolio"
-        className='w-full h-screen bg-gradient-to-t from-[#000a04] to-[#0a192f] text-white'
-      >
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-          <div className="pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-              Portfolio
-            </p>
-            <p className="py-6">Check out some of my work right here</p>
-          </div>
-  
-          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-            {portfolios.map(({ id, src, text }) => (
-              <div key={id} className="shadow-md shadow-gray-600 rounded-lg backdrop-blur-sm bg-white/30 ...">
-                <img
-                    
-                  src={src}
-                  alt=""
-                  className="rounded-md duration-200 scale-90 hover:scale-100 overflow-visible ..."
-                />
-                <div className="flex flex-col items-center justify-center">
-                  
-                  <div className="shadow-inner ...">
-                  <button className="w-1/2 px-6 py-2 m-3 mt-4 duration-200 hover:scale-105 hover:text-pink-500">
-                    Code
-                  </button>
-                  </div>
+      name="portfolio"
+      className="bg-gradient-to-b from-[#0a192f] to-gray-900 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
 
-                  <h2 className="mb-2">{text}</h2>
-                </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src,text }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg ">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex flex-col items-center justify-center">
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 ">
+                  {text}
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
-    );
+    </div>
+  );
   };
   
   export default Portfolio;
